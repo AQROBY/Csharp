66a69b2217599e8b8dacceb9cfb348bb53cdd3ef
Robert <AQ_ROBY@YAHOO.COM>
2021-01-23 02:51:24 +0200

    Initial commit

---
+++ 1. Convert a number from base 10 to base 2/FirstApplication/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FirstApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            string inputString = Console.ReadLine();
+            int input;
+
+            if (!int.TryParse(inputString, out input))
+            {
+                Console.WriteLine("The program converts only positive integers.");
+            }
+            else if (input < 0)
+            {
+                Console.WriteLine("The program converts only positive integers.");
+            }
+            else
+            {
+                const int baseTwo = 2;
+                string result = "";
+                while (input >= 0)
+                {
+                    int resultBit = input % 2;
+                    result += resultBit;
+                    input /= baseTwo;
+                    if (input == 0)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine(Reverse(result));
+            }
+        }
+
+        static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ 1. Convert a number from base 10 to base 2/README.md
@@ -0,0 +1,15 @@
+# Convert a number from base 10 to base 2
+Write an app that converts a positive integer from base 10 to base 2.
+
+Example:
+Input:
+45
+Output:
+101101
+
+Example:
+Input:
+-45
+Output:
+"The program converts only positive integers."
+
