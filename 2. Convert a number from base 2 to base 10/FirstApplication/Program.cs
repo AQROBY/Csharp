7de71026efcc65dbed1d83d8c7d900055efe1250
Robert <AQ_ROBY@YAHOO.COM>
2021-01-26 00:30:33 +0200

    Problem 2 solved

---
+++ 2. Convert a number from base 2 to base 10/FirstApplication/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace FirstApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            int operationSelect = Convert.ToInt32(Console.ReadLine());
+            string inputString = Console.ReadLine();
+            long input;
+            const int magicNumberTwo = 2;
+
+            if (!long.TryParse(inputString, out input))
+            {
+                Console.WriteLine("The program converts only positive integers.");
+            }
+            else if (input < 0)
+            {
+                Console.WriteLine("The program converts only positive integers.");
+            }
+            else
+            {
+                if (operationSelect == 1)
+                {
+                    ConvertToBaseTwo(input);
+                }
+                else if (operationSelect == magicNumberTwo)
+                {
+                    ConvertToBaseTen(Convert.ToString(inputString));
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation.");
+                }
+            }
+        }
+
+        static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        static void ConvertToBaseTwo(long input)
+        {
+            const int baseTwo = 2;
+            string result = "";
+            while (input >= 0)
+            {
+                long resultBit = input % 2;
+                result += resultBit;
+                input /= baseTwo;
+                if (input == 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(Reverse(result));
+        }
+
+        static void ConvertToBaseTen(string input)
+        {
+            string inputString = Convert.ToString(input);
+
+            foreach (char elem in input.ToCharArray(0, input.Length))
+            {
+                if (elem != '0' && elem != '1')
+                {
+                    Console.WriteLine("A valid binary number was not entered (only 0's and 1's).");
+                    return;
+                }
+            }
+
+            int result = Convert.ToInt32(inputString, 2);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 2. Convert a number from base 2 to base 10/README.md
@@ -0,0 +1,37 @@
+# Convert a number from base 2 to base 10
+Extend the previous app so now you have another operation included in it.
+The new operation converts a positive integer from base 2 to base 10.
+
+On the first line the app gets the operation code:
+1 - conversion from 10 to base 2
+2 - conversion from base 2 to base 10
+
+On the next line, the number that should be converted will be entered.
+
+Example:
+Input:
+1
+45
+Output:
+101101
+
+Example:
+Input:
+2
+101101
+Output:
+45
+
+Example:
+Input:
+2
+1021
+Output:
+A valid binary number was not entered (only 0's and 1's).
+
+Example:
+Input:
+0
+101101
+Output:
+Invalid operation.
